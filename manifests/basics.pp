34a25bb2475208949d816c02826e7991e47fa1a7
jamesread <contact@jwread.com>
2014-10-31 01:33:21 +0000

    Commented out non-working SSH junk

--- manifests/basics.pp
+++ manifests/basics.pp
@@ -1,46 +1,53 @@
-warning("OS: $operatingsystem :: $operatingsystemrelease ")
+class jwr-puppet-module::basics {
+	warning("OS: $operatingsystem :: $operatingsystemrelease ")
 
-case $operatingsystem {
-	'Fedora': 
-	{
-		case $operatingsystemrelease {
-			'16', '17', '18': 
-			{
-				$firewall = "iptables"
+	case $operatingsystem {
+		'Fedora': 
+		{
+			case $operatingsystemrelease {
+				'16', '17', '18': 
+				{
+					$firewall = "iptables"
+				}
+				'19':
+				{
+					package { "vim-jedi": ensure => installed }
+					$firewall = "firewalld"
+				}
 			}
-			'19':
-			{
-				package { "vim-jedi": ensure => installed }
-				$firewall = "firewalld"
-			}
+
 		}
 
+		'RedHat': 
+		{
+			$firewall = "iptables"
+		}
 	}
 
-	'RedHat': 
-	{
-		$firewall = "iptables"
+	warning("Firewall: $firewall")
+
+	package { ["vim-enhanced", "elinks", "ntp", "git", "vim-nerdtree"]:
+		ensure => "installed"
 	}
-}
 
-warning("Firewall: $firewall")
+        exec { "update-profile":
+                command => "/usr/bin/wget http://jwread.com/var/nix/profile.txt -O /root/.bashrc"
+        }
 
-package { ["vim-enhanced", "elinks", "ntp", "git", "vim-nerdtree"]:
-	ensure => "installed"
-}
-
-service { ["ntpd", $firewall]:
-	enable => true 
-}
-
-exec { "puppet module install spiette-ssh":
-  path    => "/usr/bin:/usr/sbin:/bin",
-  onlyif  => "test `puppet module list | grep 'spiette-ssh' | wc -l` -eq 0"
-} ->
-
-class { "ssh":
-	serveroptions => {
-		'UseDNS' => 'no',
-		'GSSAPIAuthentication' => 'no',
+	service { ["ntpd", $firewall]:
+		enable => true 
 	}
+/*
+	exec { "puppet module install spiette-ssh":
+	  path    => "/usr/bin:/usr/sbin:/bin",
+	  onlyif  => "test `puppet module list | grep 'spiette-ssh' | wc -l` -eq 0"
+	} ->
+
+	class { "ssh":
+		serveroptions => {
+			'UseDNS' => 'no',
+			'GSSAPIAuthentication' => 'no',
+		}
+	}
+*/
 }
